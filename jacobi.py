fe90c4dd7c01f807dc84ca3f558baf5e64075d51
Gabriel Pitali <gabrielpituca@gmail.com>
2016-11-29 21:12:08 -0200

    Corrigidos alguns erros no método de Jacobi, mas ainda não foi obtido o resultado esperado.

--- jacobi.py
+++ jacobi.py
@@ -5,7 +5,7 @@ import math
 
 def jacobi(q, x, h, y, tol, n_max, n):
     w_0 = [0] * (n - 1)
-    w_new = w_0
+    w_new = w_0.copy()
     w_old = []
     for i in range(n - 1):
         w_old.append(w_0[i] + 2 * tol)
@@ -13,13 +13,14 @@ def jacobi(q, x, h, y, tol, n_max, n):
     j = 0
     while norma > tol and j < n_max:
         j += 1
-        w_old = w_new
+        w_old = w_new.copy()
         w_new[0] = (1 / (2 + math.pow(h, 2) * q(x[0]))) * (y(x[0]) + w_old[1])
         w_new[n - 2] = (1 / (2 + math.pow(h, 2) * q(x[n - 2]))) * (y(x[n - 2]) + w_old[n - 3])
-        for i in range(1, n - 3):
+        for i in range(1, n - 2):
             w_new[i] = (1 / (2 + math.pow(h, 2) * q(x[i]))) * (y(x[i]) + w_old[i - 1] + w_old[i + 1])
         for i in range(n - 1):
             norma += math.pow((w_new[i] - w_old[i]), 2)
+        norma = math.sqrt(norma)
     return w_new
 
 
