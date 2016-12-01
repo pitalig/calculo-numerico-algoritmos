c1adb4332b3bae009aa651886e0dbad49a6bb050
Gabriel Pitali <gabrielpituca@gmail.com>
2016-12-01 13:42:33 -0200

    Corrige erros no interpolador de lagrange, que agora está funcionando de forma correta.

--- lagrange.py
+++ lagrange.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as mat
+from numpy import arange
 
 
-def polinomio_interp(y, x):
-    l = 0
-    for i in range(len(y)):
-        num = 1
-        den = 1
-        for k in range(len(x)):
-            if i != k:
-                num *= x - x[k]
-                den *= x[i] - x[k]
-        l += y[i] * num / den
-    return l
+# Calcula o vetor solução (y), para um intervalo de pontos de x, baseado em um conjunto resultados já conhecidos.
+# Entradas: lista de pontos de x já conhecidos, solução y correspondente aos pontos x já conhecidos,
+# intervalo de pontos de x para onde se deseja obter a solução.
+# Retorno: vetor solução para o intervalo escolhido
+def interpola(x, y, x_new):
+    y_new = []
+    # Itera por todos os elementos do vetor x desejado
+    for x_i in x_new:
+        # Calcula o polinômio interpolador e sua solução
+        l = 0
+        for i in range(len(y)):
+            num = 1
+            den = 1
+            for k in range(len(x)):
+                if i != k:
+                    num *= x_i - x[k]
+                    den *= x[i] - x[k]
+            if den != 0:
+                l += y[i] * num / den
+        y_new.append(l)
+    return y_new
 
 if __name__ == "__main__":
-    points = [(-1, 0), (0, -1), (1, 0)]
-    x_list = range(-10, 10)
-    y_list = [interpolador(points, x) for x in range(-10, 10)]
+    # Teste 1
+    points_x = [-1, 0, 1]
+    points_y = [0, -1, 0]
+    x_list = arange(-10, 10, 0.1)
+    y_list = interpola(points_x, points_y, x_list)
 
     mat.plot(x_list, y_list)
     mat.grid(True)
